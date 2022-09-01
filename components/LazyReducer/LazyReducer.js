197e623e4e0bbcd530aee5efc34e8ebc62664aa3
Alex Kio <alexandermartelkio@gmail.com>
2022-09-01 16:26:01 -0400

    Prettify

--- components/LazyReducer/LazyReducer.js
+++ components/LazyReducer/LazyReducer.js
@@ -1,40 +1,49 @@
 import { useReducer } from "react";
 
 function init(initialCount) {
-  return {count: initialCount};
+  return { count: initialCount };
 }
 
 function reducer(state, action) {
   switch (action.type) {
-    case 'increment':
-      return {count: state.count + 1};
-    case 'decrement':
-      return {count: state.count - 1};
-    case 'reset':
+    case "increment":
+      return { count: state.count + 1 };
+    case "decrement":
+      return { count: state.count - 1 };
+    case "reset":
       return init(action.payload);
     default:
       throw new Error();
   }
 }
 
-export default function LazyReducerComponent({initialCount}) {
+export default function LazyReducerComponent({ initialCount }) {
   const [state, dispatch] = useReducer(reducer, initialCount, init);
 
   return (
     <>
       <h2>
-        <a href="https://reactjs.org/docs/hooks-reference.html#lazy-initialization" target="_blank" rel="noopener noreferrer">
+        <a
+          href="https://reactjs.org/docs/hooks-reference.html#lazy-initialization"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Lazy Initialized Reducer
         </a>
       </h2>
       Count: {state.count}
       <button
-        onClick={() => dispatch({type: 'reset', payload: initialCount})}>
+        onClick={() => dispatch({ type: "reset", payload: initialCount })}
+      >
         Reset
       </button>
-      <button onClick={() => dispatch({type: 'decrement'})}>-</button>
-      <button onClick={() => dispatch({type: 'increment'})}>+</button>
-      <p>It lets you extract the logic for calculating the initial state outside the reducer. This is also handy for resetting the state later in response to an action</p>
+      <button onClick={() => dispatch({ type: "decrement" })}>-</button>
+      <button onClick={() => dispatch({ type: "increment" })}>+</button>
+      <p>
+        It lets you extract the logic for calculating the initial state outside
+        the reducer. This is also handy for resetting the state later in
+        response to an action
+      </p>
     </>
-  )
+  );
 }
--- components/Reducer/Reducer.js
+++ components/Reducer/Reducer.js
@@ -1,13 +1,13 @@
 import { useReducer } from "react";
 
-const initialState = {count: 0};
+const initialState = { count: 0 };
 
 function reducer(state, action) {
   switch (action.type) {
-    case 'increment':
-      return {count: state.count + 1};
-    case 'decrement':
-      return {count: state.count - 1};
+    case "increment":
+      return { count: state.count + 1 };
+    case "decrement":
+      return { count: state.count - 1 };
     default:
       throw new Error();
   }
@@ -19,15 +19,27 @@ export default function ReducerComponent() {
   return (
     <>
       <h2>
-        <a href="https://reactjs.org/docs/hooks-reference.html#usereducer" target="_blank" rel="noopener noreferrer">
-          Reducer
+        <a
+          href="https://reactjs.org/docs/hooks-reference.html#usereducer"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          useReducer
         </a>
       </h2>
       Count: {state.count}
-      <button onClick={() => dispatch({type: 'decrement'})}>-</button>
-      <button onClick={() => dispatch({type: 'increment'})}>+</button>
-      <p>An alternative to useState. Accepts a reducer of type (state, action) =&gt; newState, and returns the current state paired with a dispatch method.</p>
-      <p>useReducer is usually preferable to useState when you have complex state logic that involves multiple sub-values or when the next state depends on the previous one.</p>
+      <button onClick={() => dispatch({ type: "decrement" })}>-</button>
+      <button onClick={() => dispatch({ type: "increment" })}>+</button>
+      <p>
+        An alternative to useState. Accepts a reducer of type (state, action)
+        =&gt; newState, and returns the current state paired with a dispatch
+        method.
+      </p>
+      <p>
+        useReducer is usually preferable to useState when you have complex state
+        logic that involves multiple sub-values or when the next state depends
+        on the previous one.
+      </p>
     </>
-  )
+  );
 }
